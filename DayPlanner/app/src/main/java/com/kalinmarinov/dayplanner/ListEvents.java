8307aba33fd8949e438bfe1fde88ab063fb31cba
KMMarinov <kalo606@abv.bg>
2017-12-25 23:07:36 +0200

    Initial

---
+++ DayPlanner/app/src/main/java/com/kalinmarinov/dayplanner/ListEvents.java
@@ -0,0 +1,13 @@
+package com.kalinmarinov.dayplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListEvents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_events);
+    }
+}
