0f96330835a4f96f982c2f92f31c0f4a56b434cd
KMMarinov <kalo606@abv.bg>
2017-12-26 19:05:38 +0200

    Moving the ListEvents to views

--- DayPlanner/app/src/main/java/com/kalinmarinov/dayplanner/views/ListEvents.java
+++ DayPlanner/app/src/main/java/com/kalinmarinov/dayplanner/views/ListEvents.java
@@ -1,12 +1,13 @@
-package com.kalinmarinov.dayplanner;
+package com.kalinmarinov.dayplanner.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.kalinmarinov.dayplanner.R;
 
 public class ListEvents extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_events);
     }
