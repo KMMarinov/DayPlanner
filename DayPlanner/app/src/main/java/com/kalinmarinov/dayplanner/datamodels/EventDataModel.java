f61439c03c56aec74d54f7801ed875e0acee6516
KMMarinov <kalo606@abv.bg>
2017-12-26 19:03:55 +0200

    Implementing the DataModel for the Event

--- DayPlanner/app/src/main/java/com/kalinmarinov/dayplanner/datamodels/EventDataModel.java
+++ DayPlanner/app/src/main/java/com/kalinmarinov/dayplanner/datamodels/EventDataModel.java
@@ -1,5 +1,6 @@
 package com.kalinmarinov.dayplanner.datamodels;
 
+import com.kalinmarinov.dayplanner.database.datasource.EventDataSource;
 import com.kalinmarinov.dayplanner.models.Event;
 
 import java.util.List;
@@ -9,13 +10,21 @@ import java.util.List;
  */
 public class EventDataModel {
 
+    private final EventDataSource eventDataSource;
+
+    public EventDataModel(final EventDataSource eventDataSource) {
+        this.eventDataSource = eventDataSource;
+    }
+
     public List<Event> getEvents() {
-        return null;
+        return eventDataSource.findAll();
     }
 
     public void deleteEvent(final Event event) {
+        eventDataSource.delete(event);
     }
 
     public void saveEvent(final Event event) {
+        eventDataSource.save(event);
     }
 }
